7b8cb304d25b6102aab9aa6a198e4bd876c694f2
Steve Mask <mnicpt@gmail.com>
2015-05-20 23:59:43 -0500

    Create cover and choice view controllers

---
+++ SMARTePARK/SMARTePARK/ChoiceViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ChoiceViewController.swift
+//  SMARTePARK
+//
+//  Created by Steven Mask on 5/20/15.
+//  Copyright (c) 2015 Steven Mask. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ChoiceViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        
+        if locationManager.respondsToSelector("requestWhenInUseAuthorization") {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        locationManager.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        }
+    }
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SMARTePARK/SMARTePARK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SMARTePARK
+//
+//  Created by Steven Mask on 5/20/15.
+//  Copyright (c) 2015 Steven Mask. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
